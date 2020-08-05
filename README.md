22551a2e3ddacfe1abea9675e78fd2e81d644e6f
RDeveloperTrial <adevelopertestingdevice@gmail.com>
2020-08-05 22:15:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-e-commerce-barista
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-e-commerce-barista)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'Ariete Vintage',
+    price: 165,
+    description: 'Expresso machine with 900W power and 15 bar pression',
+    details: 'Elegant and stylish design, in vintage style, '
+  },
+  {
+    name: 'Ariete Retro',
+    price: 125,
+    description: 'Expresso machine, Model: 1388/31',
+    details: 'Steel body in metal finish, toggle switches with indicator lights, temperature gauge, quality steaming and frothing with maxi cappucino device.'
+  },
+  {
+    name: 'Ariete Classica',
+    price: 135,
+    description: '',
+    details: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
